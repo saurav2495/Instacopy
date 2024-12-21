623472908c0685feac74ff3bdebd3f23ac9b3b68
saurav2495 <sauravkumar6791@gmail.com>
2024-12-21 22:41:51 +0530

    added show feature

--- script.js
+++ script.js
@@ -2,41 +2,42 @@ var arr = [
   {
     dp: "https://images.unsplash.com/photo-1730941343980-5d81ce7c768b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8",
     story:
-      "https://images.unsplash.com/photo-1732806685351-bafb9848cebf?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzNnx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1734568365466-5a551e6857a7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8UzRNS0xBc0JCNzR8fGVufDB8fHx8fA%3D%3D",
   },
   {
     dp: "https://images.unsplash.com/photo-1733910087506-f634f8a806de?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0N3x8fGVufDB8fHx8fA%3D%3D",
     story:
-      "https://images.unsplash.com/photo-1734188515104-5f036a02f551?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3NHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1732209988927-396f5103ede8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDN8UzRNS0xBc0JCNzR8fGVufDB8fHx8fA%3D%3D",
   },
   {
     dp: "https://images.unsplash.com/photo-1734533992947-44970e6e5010?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2MHx8fGVufDB8fHx8fA%3D%3D",
     story:
-      "https://images.unsplash.com/photo-1733126916745-ae6453f1fe4e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2NXx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1734298497319-bd264ccf0c22?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDExfFM0TUtMQXNCQjc0fHxlbnwwfHx8fHw%3D",
   },
   {
     dp: "https://images.unsplash.com/photo-1730941343980-5d81ce7c768b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8",
     story:
-      "https://images.unsplash.com/photo-1732806685351-bafb9848cebf?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzNnx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1733356214560-be25f2df4714?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI2fFM0TUtMQXNCQjc0fHxlbnwwfHx8fHw%3D",
   },
   {
     dp: "https://images.unsplash.com/photo-1733910087506-f634f8a806de?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0N3x8fGVufDB8fHx8fA%3D%3D",
     story:
-      "https://images.unsplash.com/photo-1734188515104-5f036a02f551?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3NHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1611265023526-707d240623af?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDM5fFM0TUtMQXNCQjc0fHxlbnwwfHx8fHw%3D",
   },
   {
     dp: "https://images.unsplash.com/photo-1734533992947-44970e6e5010?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2MHx8fGVufDB8fHx8fA%3D%3D",
     story:
-      "https://images.unsplash.com/photo-1733126916745-ae6453f1fe4e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2NXx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1731589815860-2a5665633d93?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDU1fFM0TUtMQXNCQjc0fHxlbnwwfHx8fHw%3D",
   },
 ];
 
 let sum = "";
-arr.forEach((elem) => {
+arr.forEach((elem, idx) => {
   sum += `
-  
+
      <div class="story">
           <img
+          id="${idx}"
             src="${elem.dp}"
             alt=""
           />
@@ -45,4 +46,13 @@ arr.forEach((elem) => {
 });
 
 var storeyan = document.querySelector(".storeyan");
-storeyan.innerHTML = sum;
+var full = document.querySelector(".full");
+storeyan.innerHTML += sum;
+
+storeyan.addEventListener("click", (dets) => {
+  var value = arr[dets.target.id].story;
+  console.log(value);
+
+  full.style.display = "block";
+  full.style.backgroundImage = `url(${value})`;
+});
